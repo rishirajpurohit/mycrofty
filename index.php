0716015dc0756aaad39cc1f22eca46485c4dc1e2
Rishiraj Purohit <purohitji007@gmail.com>
2018-04-15 23:55:00 +0530

    Added language intent parser

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
-echo "hi";
 
+$data = file_get_contents("php://input");
+$fh = fopen(__DIR__."/logs.txt","a+");
+fwrite($fh,$data."\n\n\r");
+fclose($fh);
 
 ?>
---
+++ parser.py
@@ -0,0 +1,48 @@
+__author__ = 'Rishiraj'
+
+import json
+import sys
+from adapt.intent import IntentBuilder
+from adapt.engine import IntentDeterminationEngine
+
+engine = IntentDeterminationEngine()
+
+weather_keyword = [
+    "weather"
+]
+
+for wk in weather_keyword:
+    engine.register_entity(wk, "TimeKeyword")
+
+weather_types = [
+    "snow",
+    "rain",
+    "wind",
+    "sleet",
+    "sun"
+]
+
+for wt in weather_types:
+    engine.register_entity(wt, "WeatherType")
+
+locations = [
+    "Seattle",
+    "San Francisco",
+    "Tokyo"
+]
+
+for loc in locations:
+    engine.register_entity(loc, "Location")
+
+weather_intent = IntentBuilder("WeatherIntent")\
+    .require("TimeKeyword")\
+    .optionally("WeatherType")\
+    .optionally("Location")\
+    .build()
+
+engine.register_intent_parser(weather_intent)
+
+if __name__ == "__main__":
+    for intent in engine.determine_intent(' '.join(sys.argv[1:])):
+        if intent.get('confidence') > 0:
+            print(json.dumps(intent, indent=4))
